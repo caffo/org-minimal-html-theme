e3a2fe0cc93465105a3ace1c50282499346cadda
Rafael Rinaldi <rafael@rinaldi.io>
2016-02-29 22:47:08 -0300

    feat(checklists): add full checklists support :tada:
    Add full checklists support with the help of a simple JavaScript
    snippet.
    
    Closes #5

---
+++ src/scripts/index.js
@@ -0,0 +1,44 @@
+(function (exports) {
+  'use strict';
+
+  // Query selector helper
+  function $(selector) {
+    var lookup = exports.document.querySelectorAll(selector);
+    return [].slice.call(lookup);
+  }
+
+  // Setup checklists
+  function setupChecklists() {
+    // Remove `<code>` from `li.on` and `li.off`
+    $('li[class^=o] code').forEach(function (node, index) {
+      var ul = node.parentNode.parentNode;
+      var li = node.parentNode;
+      var code = node;
+      var isChecked = li.classList.contains('on');
+      var checkbox = createCheckbox(isChecked);
+
+      // Add `checklist` class to the list
+      ul.classList.add('checklist');
+
+      // Insert a checkbox element to the list item
+      li.insertBefore(checkbox, code);
+
+      // Remove code node
+      code.remove();
+    });
+  }
+
+  // Creates a simple checkbox element
+  // isChecked: Whether or not it should display checked
+  function createCheckbox(isChecked) {
+    var checkbox = exports.document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('disabled', 'disabled');
+
+    if (isChecked) checkbox.setAttribute('checked', 'checked');
+
+    return checkbox;
+  }
+
+  setupChecklists();
+})(window);
